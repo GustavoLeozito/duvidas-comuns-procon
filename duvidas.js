3a7343f46ef391292be4850598af380e22e51c5a
GustavoAlmeid <45299353+GustavoLeozito@users.noreply.github.com>
2020-04-06 19:12:36 -0300

    Update duvidas.js

--- duvidas.js
+++ duvidas.js
@@ -1,1 +1,25 @@
+$(window).on('load', async () => {
+    loadTableData('duvidas');
+    // btnArea();
+})
 
+$('#escolha').on('click', (e) => {
+    $('#escolhaInfo').toggleClass('hide');
+})
+class Item {
+    constructor(perguntas, respostas) {
+        this.perguntas = pergunta;
+        this.resposta = resposta;
+        
+       
+    }
+}
+
+async function loadTableData(query) {
+    var url = ``;
+    
+    let i = 0;
+    return await $.getJSON(url, (data) => {
+        const duvidas = {}
+        
+        
